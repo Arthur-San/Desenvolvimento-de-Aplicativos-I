62ae88797d837d3982eba070080572a17f40e55d
Arthur dos Santos <67018260+Arthur-San@users.noreply.github.com>
2021-04-06 22:54:54 -0300

    Aula22

---
+++ Aulas/Aula22/app/src/main/java/com/example/menudelista/CursoJavaScript.java
@@ -0,0 +1,14 @@
+package com.example.menudelista;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CursoJavaScript extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_curso_java_script);
+    }
+}
--- Aulas/Aula22/app/src/main/java/com/example/menudelista/MainActivity.java
+++ Aulas/Aula22/app/src/main/java/com/example/menudelista/MainActivity.java
@@ -3,12 +3,46 @@ package com.example.menudelista;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    //1º Etapa: Declarar as variáveis e a construção do nosso Array
+    private ListView lista;
+    private String [] itens = {"HTML5", "CSS", "SASS", "JavaScript", "NodeJs", "AngularJs", "Ruby", "React", "jQuery"};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //2º Etapa: Fazer o mapeamento da Lista
+        lista = findViewById(R.id.lista);
+
+        //3º Etapa: Crirar um adaptador para servir de ligação entre o Array e o elemento ListView no XML
+        //O adaptador vem do atributo ArrayAdapter
+        //android.R.layout.simple_list_item_1: Mapeamento que Define o tipo da lista, ou seja, uma lista simples no layout atual - item 1;
+        //android.R.id.text1, itens: Mapeamento que Define os tipos de valores da lista
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getApplicationContext(),
+                android.R.layout.simple_list_item_1, android.R.id.text1, itens);
+
+
+        //4º Etapa: Adicionar o adapter para a lista criada no Array
+        lista.setAdapter(adapter);
+
+        //5º Etapa: Print da tela em Toast indicado qual o item que foi clicado
+        //eventos: setOnItemClickListener e onItemClick
+        lista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int i, long id) {
+                Toast.makeText(MainActivity.this, itens[i], Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
     }
 }
