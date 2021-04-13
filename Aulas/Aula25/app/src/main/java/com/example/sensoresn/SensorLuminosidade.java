7fc01e94e59ee09dfced1efb1629cb0b967ebef8
Arthur dos Santos <67018260+Arthur-San@users.noreply.github.com>
2021-04-12 22:38:35 -0300

    Aula25

---
+++ Aulas/Aula25/app/src/main/java/com/example/sensoresn/SensorLuminosidade.java
@@ -0,0 +1,91 @@
+package com.example.sensoresn;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.sensoresn.MainActivity;
+
+public class SensorLuminosidade extends AppCompatActivity {
+    private TextView visual;
+    SensorManager sm;
+    SensorEventListener listener;
+    Sensor luz;
+    Button btnAce;
+    Button btnVoltarProx;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        visual = findViewById(R.id.visual);
+        sm = (SensorManager) getSystemService(SENSOR_SERVICE);
+        luz = sm.getDefaultSensor(Sensor.TYPE_LIGHT);
+        btnAce = findViewById(R.id.btnAce);
+        btnVoltarProx = findViewById(R.id.btnVoltarProx);
+
+        btnAce.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                abriAcele();
+            }
+        });
+
+        btnVoltarProx.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                voltar();
+            }
+        });
+        listener = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent event)
+            {
+                visual.setText(String.valueOf(event.values[0]));
+                int grayShade = (int) event.values[0];
+
+                if(grayShade > 255) grayShade = 255;
+
+                visual.setTextColor(Color.rgb( 255 - grayShade, 255 - grayShade, 255 - grayShade ) );
+                visual.setBackgroundColor(Color.rgb(grayShade,grayShade,grayShade));
+
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int i)
+            {
+
+            }
+        };
+
+        sm.registerListener(listener,luz,SensorManager.SENSOR_DELAY_FASTEST);
+
+    }
+
+    public void abriAcele(){
+        Intent janela = new Intent(this, SensorManager.class);
+        startActivity(janela);
+    }
+
+    public void voltar(){
+        Intent janelaV = new Intent(this, MainActivity.class);
+        startActivity(janelaV);
+    }
+    @Override
+    protected void onPause() {
+
+        sm.unregisterListener(listener,luz);
+        super.onPause();
+    }
+}
